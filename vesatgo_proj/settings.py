e7f2768d6a445a211bde540f7f343cffe52d2f37
nikhil deshmukh <deshm.nikhil@gmail.com>
2020-08-25 19:16:34 +0530

    github

--- vesatgo_proj/settings.py
+++ vesatgo_proj/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'vesatgo_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'vesatgodb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'nikpg',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vesatgodb',
+        'USER': 'postgres',
+        'PASSWORD': 'nikpg',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
